b23b5e3f1e4708132c4a2e4c3c9845ae4af0505f
Sai Krishna Reddy Kankanala <saikrishnareddykankanala@gmail.com>
2025-05-02 18:18:58 -0500

    Remove root option from vite.config.ts for Vercel compatibility

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.', // This is important for Vercel
   build: {
     outDir: 'dist',
     emptyOutDir: true,
